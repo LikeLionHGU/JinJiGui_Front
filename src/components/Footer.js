bb748f982d1618dd307fcd776b9704da84d63430
playoh <wazoski05@gmail.com>
2025-02-08 00:14:02 +0900

    #1 yeonju_style: 배경 색상 및 선 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import "../components/styles/Footer.css";
 
 function Footer () {
   return (
     <div>
-      <div> 푸터입니다. </div>
+      <hr className="Footer_Line"/>
+      <div className="Footer_Container">
+        <div className="Footer_Left">
+          <span>LOGO</span>
+          <span>COMPANY: MOBOGGA</span>
+          <span>ADRESS: Jongno 3-gil, Jongno-gu, Seoul</span>
+          <span>CALL: 010-8507-5949</span>
+        </div>
+
+        <div className="Footer_Right">
+          <span>Company introduction</span>
+          <span>Recruitment of talent</span>
+          <span>Affiliate proposal</span>
+          <span>Terms and Conditions of Use</span>
+          <span>Personal Information Processing Policy</span>
+          <span>Policy Customer Center</span>
+      </div>
+      </div>
     </div>
   )
 }
