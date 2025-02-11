677e33761389f62b5e7d01f2cbb26eafffa0ad87
lingardd09 <sherlockicker@gmail.com>
2025-02-12 03:30:07 +0900

    #3/wonjin_chore: 마우스 호버 시 cursor 모양 pointer로 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import "../components/styles/Header.css"
-function Header () {
-
+import "../components/styles/Header.css";
+function Header() {
   const navigate = useNavigate();
 
   const loginEnter = () => {
-    navigate('/login')
+    navigate("/login");
   };
   const homeEnter = () => {
-    navigate('/')
+    navigate("/");
   };
 
   return (
     <div>
       <div className="Header_Container">
-
-      <div className="Header_Left">
-          <span onClick={homeEnter}>LOGO</span>
+        <div className="Header_Left">
+          <span className="Header_Link" onClick={homeEnter}>LOGO</span>
         </div>
-        
+
         <div className="Header_Right">
-          <span onClick={homeEnter}>홈</span>
-          <span onClick={loginEnter}>로그인</span>
-          <span>회원가입</span>
+          <span className="Header_Link" onClick={homeEnter}>
+            홈
+          </span>
+          <span className="Header_Link" onClick={loginEnter}>
+            로그인
+          </span>
+          <span className="Header_Link" onClick={loginEnter}>
+            회원가입
+          </span>
         </div>
       </div>
-      <hr className="Header_Line"/>
+      <hr className="Header_Line" />
     </div>
-  )
+  );
 }
 
 export default Header;
