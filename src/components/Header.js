040363d60c883684b1753d6ec542f537aaa9919a
junnyang <lhr8440@gmail.com>
2025-02-13 00:10:04 +0900

    merge: main merge 후 conflict 해결

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import "../components/styles/Header.css";
 function Header() {
-<<<<<<< HEAD
   const [notLoggedIn, setNotLoggedIn] = useState(false);
   const [manager, setManager] = useState(true);
 
-=======
->>>>>>> main
   const navigate = useNavigate();
 
   const loginEnter = () => {
@@ -21,26 +18,40 @@ function Header() {
     <div>
       <div className="Header_Container">
         <div className="Header_Left">
-          <span className="Header_Link" onClick={homeEnter}>LOGO</span>
+          <span className="Header_Link" onClick={homeEnter}>
+            LOGO
+          </span>
         </div>
 
         {notLoggedIn ? (
           <div className="Header_Right">
-            <span className="Header_Link" onClick={homeEnter}>홈</span>
-            <span className="Header_Link" onClick={loginEnter}>로그인</span>
+            <span className="Header_Link" onClick={homeEnter}>
+              홈
+            </span>
+            <span className="Header_Link" onClick={loginEnter}>
+              로그인
+            </span>
             <span>회원가입</span>
           </div>
         ) : manager ? (
           <div className="Header_Right">
-            <span className="Header_Link" onClick={loginEnter}>관리자 페이지</span>
-            <span className="Header_Link" onClick={homeEnter}>홈</span>
+            <span className="Header_Link" onClick={loginEnter}>
+              관리자 페이지
+            </span>
+            <span className="Header_Link" onClick={homeEnter}>
+              홈
+            </span>
             <span>로그아웃</span>
             <span>마이페이지</span>
           </div>
         ) : (
           <div className="Header_Right">
-            <span className="Header_Link" onClick={homeEnter}>홈</span>
-            <span className="Header_Link" onClick={loginEnter}>로그아웃</span>
+            <span className="Header_Link" onClick={homeEnter}>
+              홈
+            </span>
+            <span className="Header_Link" onClick={loginEnter}>
+              로그아웃
+            </span>
             <span>마이페이지</span>
           </div>
         )}
