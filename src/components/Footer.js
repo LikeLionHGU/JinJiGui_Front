5a5bb0c08d6a904127bb7e66334295b419259066
lingardd09 <sherlockicker@gmail.com>
2025-02-13 03:23:22 +0900

    style: 푸터 디자인 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,21 @@
 import React from "react";
 import "../components/styles/Footer.css";
+import main_logo from "../assets/main_logo.svg";
 
-function Footer () {
+function Footer() {
   return (
     <div className="Footer">
-      <hr className="Footer_Line"/>
+      <div className="Footer_Space"></div>
       <div className="Footer_Container">
-        <span className="Footer_Left">
-          <ul>
-            <li>LOGO</li>
-            <li>COMPANY: MOBOGGA</li>
-            <li>ADRESS: Jongno 3-gil, Jongno-gu, Seoul</li>
-            <li>CALL: 010-8507-5949</li>
-          </ul>
+        <img className="Footer_Content" src={main_logo} alt="로고 이미지" />
+        <span className="Footer_Content">회사명: MOBOGGA</span>
+        <span className="Footer_Content">
+          주소: 경상북도 포항시 북구 흥해읍 한동로 558, 한동대학교
         </span>
-
-        <div className="Footer_Right">
-          <ul>
-            <li>Company introduction</li>
-            <li>Recruitment of talent</li>
-            <li>Affiliate proposal</li>
-            <li>Terms and Conditions of Use</li>
-            <li>Personal Information Processing Policy</li>
-            <li>Policy Customer Center</li>
-          </ul>
-        </div>
+        <span className="Footer_Content">연락처: [대장님 전화번호]</span>
       </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
