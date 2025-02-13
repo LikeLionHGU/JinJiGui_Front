afd22fb870c2a6aec819ac7675611be72e460101
lingardd09 <sherlockicker@gmail.com>
2025-02-13 23:11:45 +0900

    개인정보 수정페이지 초기제작 및 서비스 타이틀 적용, 폰트 적용

--- src/pages/UpdateProfile.js
+++ src/pages/UpdateProfile.js
@@ -1,13 +1,41 @@
 import React from "react";
 import SubHeader from "../components/SubHeader";
+import login_logo from "../assets/login_logo.svg"
 import "./styles/UpdateProfile.css"
 
 function UpdateProfile () {
   return (
-    <div>
+    <>
       <SubHeader />
-      <div> 프로필 수정 페이지입니다. </div>
-    </div>
+      <div className="update-page-body">
+        <div className="update-page-box">
+          <div className="update-page-logo-box">
+            <img src={login_logo} alt="서비스_로고"/>
+          </div>
+          <div className="update-page-title-box">
+            <div className="update-page-title">
+              회원정보 수정
+            </div>
+          </div>
+          <div className="update-page-content-box">
+            <div className="update-page-content">
+
+            </div>
+            <div className="update-page-content">
+              
+            </div>
+            <div className="update-page-content">
+              
+            </div>
+
+          </div>
+          <div className="update-page-saveButton-box">
+
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
