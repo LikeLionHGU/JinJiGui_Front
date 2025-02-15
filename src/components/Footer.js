0ea9de6dc2a84e09e68ba329422d38161297fc4d
lingardd09 <sherlockicker@gmail.com>
2025-02-15 14:49:20 +0900

    푸터 배치 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,11 @@ function Footer() {
     <div className="Footer">
       <div className="Footer_Space"></div>
       <div className="Footer_Container">
-        <img className="Footer_Content" src={main_logo} alt="로고 이미지" />
-        <span className="Footer_Content">회사명: MOBOGGA</span>
+        <span className="Footer_Content">회사명: MOBOGGA | 연락처: 010-1234-1234</span>
         <span className="Footer_Content">
           주소: 경상북도 포항시 북구 흥해읍 한동로 558, 한동대학교
         </span>
-        <span className="Footer_Content">연락처: [대장님 전화번호]</span>
+        <img className="Footer_Content" src={main_logo} alt="로고 이미지" />
       </div>
     </div>
   );
