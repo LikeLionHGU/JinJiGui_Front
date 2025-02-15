3d995ed55736e0285409e4ed169bb2687fed0a48
junnyang <lhr8440@gmail.com>
2025-02-15 22:34:07 +0900

    feat: 배너 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import Card from "../components/Card";
 import axios from "axios";
@@ -6,6 +8,7 @@ import "../components/styles/Main.css";
 
 function Main() {
   const [cards, setCards] = useState([]);
+  const [banners, setBanners] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -29,32 +32,75 @@ function Main() {
         show: {
           id: 2,
           poster:
-            "https://stqnq5ux4599.edge.naverncp.com/data2/content/image/2023/10/12/.cache/512/202310120969480.jpg",
-          title: "에픽하이",
-          startDate: "2025-12-16",
-          endDate: "2025-12-17",
+            "https://cdn.discordapp.com/attachments/1331500701808988244/1339862820443000853/IMG_6870.jpg?ex=67b0ecbd&is=67af9b3d&hm=c880832131772a86969d434a8208377496f7e9db107b44a33cf1a7eb7109f046&g",
+          title: "무도회",
+          startDate: "2024-11-23",
+          endDate: "2024-11-24",
         },
         club: {
-          name: "에픽하이",
-          category: "콘서트",
+          name: "MIC",
+          category: "댄스",
         },
       },
       {
         show: {
           id: 3,
-          poster: "https://cdn.hankyung.com/photo/202410/01.38224314.1.jpg",
-          title: "2NE1",
-          startDate: "2025-12-3",
-          endDate: "2025-12-8",
+          poster:
+            "https://cdn.discordapp.com/attachments/1331500701808988244/1339862820820615199/IMG_6871.jpg?ex=67b0ecbd&is=67af9b3d&hm=717c4b5ab530a2c1f2a6805f566436ca2ca30a92d42a90bde651bbcc6509d090&",
+          title: "THE GOSPEL: Who we are",
+          startDate: "2023-11-24",
+          endDate: "2023-11-24",
         },
         club: {
-          name: "2NE1",
+          name: "PITCH PIPE",
           category: "콘서트",
         },
       },
       {
         show: {
           id: 4,
+          poster:
+            "https://cdn.discordapp.com/attachments/1331500701808988244/1339862821202034718/IMG_6872.jpg?ex=67b0ecbe&is=67af9b3e&hm=3bcbda92ed0a10ccf0ee4ed9654063f3a52ad1f6e7a299a7ee062e1ad530b39f&",
+          title: "행복한 가족",
+          startDate: "2023-05-18",
+          endDate: "2023-05-20",
+        },
+        club: {
+          name: "어메이징 스토리",
+          category: "연극",
+        },
+      },
+      {
+        show: {
+          id: 5,
+          poster:
+            "https://cdn.discordapp.com/attachments/1331500701808988244/1339862821495771209/IMG_6873.jpg?ex=67b0ecbe&is=67af9b3e&hm=adad7ae74f938f846cffe8affa23a7353c5adf7bbf6b1b980edb346ae08dba82&",
+          title: "Re:quid",
+          startDate: "2024-11-15",
+          endDate: "2024-11-16",
+        },
+        club: {
+          name: "Repuid",
+          category: "밴드",
+        },
+      },
+      {
+        show: {
+          id: 6,
+          poster:
+            "https://cdn.discordapp.com/attachments/1331500701808988244/1339862821806145536/IMG_6874.jpg?ex=67b0ecbe&is=67af9b3e&hm=ded56ca10196628a41b0c4bd211e6fe61c6d9424ee5629739a89a505589ec55f&",
+          title: "Nostalgia: Ebenezer",
+          startDate: "2024-11-3",
+          endDate: "2024-11-4",
+        },
+        club: {
+          name: "God's Fellows",
+          category: "콘서트",
+        },
+      },
+      {
+        show: {
+          id: 7,
           poster: "https://cdn.hankyung.com/photo/202410/01.38224314.1.jpg",
           title: "2NE1",
           startDate: "2025-12-3",
@@ -68,54 +114,76 @@ function Main() {
     ],
   };
 
-  const getCards = async () => {
-    try {
-      const response = await axios.get(`https://jinjigui.info/`).then(() => {
-        console.log("API 호출 성공:", response.data);
-      }); // 응답 데이터 확인
-      const showInfo = response.data || []; // 응답 데이터가 비어있거나 null일 때 대비
-      const formattedCards = showInfo.map((info) => ({
-        id: info.show.id,
-        poster: info.show.poster,
-        title: info.show.title,
-        startDate: info.show.startDate,
-        endDate: info.show.endDate,
-        clubId: info.club.id,
-        clubName: info.club.name,
-        category: info.club.category,
-      }));
-      setCards(formattedCards);
-    } catch (error) {
-      console.error("API 호출 실패:", error);
-      setError("카드를 불러오지 못했습니다.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // const getCards = () => {
+  // const getCards = async () => {
   //   try {
-  //     const showInfo = sample_data.show_info || [];
+  //     const response = await axios.get(`https://jinjigui.info/`).then(() => {
+  //       console.log("API 호출 성공:", response.data);
+  //     }); // 응답 데이터 확인
+  //     const showInfo = response.data || []; // 응답 데이터가 비어있거나 null일 때 대비
   //     const formattedCards = showInfo.map((info) => ({
   //       id: info.show.id,
   //       poster: info.show.poster,
   //       title: info.show.title,
   //       startDate: info.show.startDate,
   //       endDate: info.show.endDate,
+  //       clubId: info.club.id,
   //       clubName: info.club.name,
   //       category: info.club.category,
   //     }));
-  //     console.log(showInfo);
   //     setCards(formattedCards);
   //   } catch (error) {
-  //     console.error("카드를 불러오지 못했습니다.", error);
+  //     console.error("API 호출 실패:", error);
   //     setError("카드를 불러오지 못했습니다.");
   //   } finally {
   //     setLoading(false);
   //   }
   // };
 
+  const handleThumbClick = (index) => {
+    console.log(`썸네일 ${index + 1} 클릭!`);
+  };
+
+  const getBanners = () => {
+    try {
+      const showInfo = sample_data.show_info || [];
+      const formattedBanners = showInfo.map((info) => ({
+        id: info.show.id,
+        poster: info.show.poster,
+      }));
+      console.log(showInfo);
+      setBanners(formattedBanners);
+    } catch (error) {
+      console.error("배너를 불러오지 못했습니다.", error);
+      setError("배너를 불러오지 못했습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getCards = () => {
+    try {
+      const showInfo = sample_data.show_info || [];
+      const formattedCards = showInfo.map((info) => ({
+        id: info.show.id,
+        poster: info.show.poster,
+        title: info.show.title,
+        startDate: info.show.startDate,
+        endDate: info.show.endDate,
+        clubName: info.club.name,
+        category: info.club.category,
+      }));
+      console.log(showInfo);
+      setCards(formattedCards);
+    } catch (error) {
+      console.error("카드를 불러오지 못했습니다.", error);
+      setError("카드를 불러오지 못했습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
+    getBanners();
     getCards();
   }, []);
 
@@ -125,7 +193,24 @@ function Main() {
   return (
     <div className="wrap">
       <div id="contents">
-        <div id="banner"></div>
+        <div id="banner">
+          <Carousel
+            className="crsl"
+            autoPlay
+            infiniteLoop
+            centerMode
+            interval={2000}
+            onClickThumb={(index) => handleThumbClick(index)}
+          >
+            {banners.map((banner) => (
+              <img
+                key={banner.id}
+                src={banner.poster}
+                alt={`Banner ${banner.id}`}
+              />
+            ))}
+          </Carousel>
+        </div>
         <div id="list-text">
           <p> 전체 공연 목록</p>
         </div>
---
+++ src/pages/ManageClub.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class ManageClub extends Component {
+  static propTypes = {};
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ManageClub;
