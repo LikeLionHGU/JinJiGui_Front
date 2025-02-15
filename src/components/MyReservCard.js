7e930aece070e699461f2da3411da902bc990f4a
lingardd09 <sherlockicker@gmail.com>
2025-02-16 02:03:23 +0900

    최근예매내역 페이지 api 제외 완성

---
+++ src/components/MyReservCard.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./styles/MyReservCard.css";
+
+function MyReservCard({
+  img_path,
+  title,
+  order,
+  date,
+  time,
+  place,
+  account,
+  ticketNum,
+  totalCost,
+  isDeposit,
+}) {
+  const getDepositStatus = () => {
+    return isDeposit ? (
+      <span style={{ color: "blue" }}>입금 완료</span>
+    ) : (
+      <span style={{ color: "red" }}>입금 전</span>
+    );
+  };
+
+  return (
+    <>
+      <div className="myreserv-card">
+        <div className="myreserv-poster-box">
+          <img src={img_path} id="myreserv-poster" alt="show_poster" />
+        </div>
+        <div className="myreserv-detail-box">
+          <div className="myreserv-detail-column" id="myreserv-detail-left-box">
+            <div className="myreserv-detail-header">제목</div>
+            <div className="myreserv-detail-header">회차</div>
+            <div className="myreserv-detail-header">날짜 | 시간</div>
+            <div className="myreserv-detail-header">장소</div>
+            <div className="myreserv-detail-header">입금 계좌</div>
+            <div className="myreserv-detail-header">티켓 매수 | 총액</div>
+            <div className="myreserv-detail-header">입금 상태</div>
+          </div>
+          <div
+            className="myreserv-detail-column"
+            id="myreserv-detail-right-box"
+          >
+            <div className="myreserv-detail-body">{title}</div>
+            <div className="myreserv-detail-body">{order}</div>
+            <div className="myreserv-detail-body">
+              {date} | {time}
+            </div>
+            <div className="myreserv-detail-body">{place}</div>
+            <div className="myreserv-detail-body">{account}</div>
+            <div className="myreserv-detail-body">
+              {ticketNum}매 | {totalCost}원
+            </div>
+            <div className="myreserv-detail-body">{getDepositStatus()}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyReservCard;
