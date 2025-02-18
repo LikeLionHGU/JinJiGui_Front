55f911b415e285bda2fee8575a51b0720e30952c
junnyang <lhr8440@gmail.com>
2025-02-18 21:34:54 +0900

    fix: 헤더 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router";
 import { NavLink } from "react-router-dom";
 import "../components/styles/Header.css";
@@ -11,6 +11,20 @@ function Header() {
   const [manager, setManager] = useState(true);
   const location = useLocation();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = sessionStorage.getItem("serverResponse");
+
+    if (userInfo) {
+      setNotLoggedIn(false);
+
+      // 사용자 정보에서 관리자 여부를 확인하는 로직 (예시)
+      const parsedUserInfo = JSON.parse(userInfo);
+      if (parsedUserInfo.isManager) {
+        setManager(true);
+      }
+    }
+  }, []);
 
   const loginEnter = () => {
     navigate("/login");
@@ -25,11 +39,8 @@ function Header() {
   //   navigate("/manager");
   // };
 
-  const activeStyle = {
-    color: "#EB5A3C",
-  };
-
   // 마이페이지 경로 체크 함수
+  // eslint-disable-next-line
   const isMyPageActive = () => {
     return location.pathname.startsWith("/mypage");
   };
@@ -48,13 +59,7 @@ function Header() {
 
         {notLoggedIn ? (
           <div className="Header_Right">
-            <NavLink
-              className="Header_Link"
-              style={({ isActive }) =>
-                isActive ? activeStyle : { color: "white" }
-              }
-              to="/"
-            >
+            <NavLink className="Header_Link" to="/">
               <span>홈</span>
             </NavLink>
             <span className="Header_Link" onClick={loginEnter}>
@@ -66,32 +71,14 @@ function Header() {
           </div>
         ) : manager ? (
           <div className="Header_Right">
-            <NavLink
-              className="Header_Link"
-              style={({ isActive }) =>
-                isActive ? activeStyle : { color: "white" }
-              }
-              to="/manager"
-            >
+            <NavLink className="Header_Link" to="/manager">
               <span>내 공연 관리</span>
             </NavLink>
-            <NavLink
-              className="Header_Link"
-              style={({ isActive }) =>
-                isActive ? activeStyle : { color: "white" }
-              }
-              to="/"
-            >
+            <NavLink className="Header_Link" to="/">
               <span>홈</span>
             </NavLink>
             <span>로그아웃</span>
-            <NavLink
-              className="Header_Link"
-              style={() =>
-                isMyPageActive() ? activeStyle : { color: "white" }
-              }
-              to="/mypage"
-            >
+            <NavLink className="Header_Link" to="/mypage">
               <span>마이페이지</span>
             </NavLink>
           </div>
@@ -101,13 +88,7 @@ function Header() {
               홈
             </span>
             <span className="Header_Link">로그아웃</span>
-            <NavLink
-              className="Header_Link"
-              style={() =>
-                isMyPageActive() ? activeStyle : { color: "white" }
-              }
-              to="/mypage/reservation"
-            >
+            <NavLink className="Header_Link" to="/mypage/reservation">
               <span>마이페이지</span>
             </NavLink>
           </div>
